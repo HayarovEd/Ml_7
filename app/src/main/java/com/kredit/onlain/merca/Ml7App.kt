444731c45d35a308731ae2a78747c4bf16b9eb64
hayarovy <edurd@rambler.ru>
2023-11-11 14:31:44 +0500

    1

---
+++ app/src/main/java/com/kredit/onlain/merca/Ml7App.kt
@@ -0,0 +1,23 @@
+package com.kredit.onlain.merca
+
+import android.app.Application
+import com.my.tracker.MyTracker
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
+import dagger.hilt.android.HiltAndroidApp
+import pro.userx.UserX
+
+@HiltAndroidApp
+class Ml7App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val config = YandexMetricaConfig.newConfigBuilder(APP_METRICA).build()
+
+
+        MyTracker.initTracker(MY_TRACKER, this)
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+        UserX.init(USER_X)
+    }
+}
